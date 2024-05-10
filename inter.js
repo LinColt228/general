749f9cc37f3211c3bb00708739882ce3e028d22f
LinColt228 <169100790+LinColt228@users.noreply.github.com>
2024-05-10 11:51:33 +0300

    Update inter.js

--- inter.js
+++ inter.js
@@ -1,15 +1,17 @@
-// JavaScript код
-document.getElementById('directionForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Зупиняємо перезавантаження сторінки при відправці форми
+ // JavaScript код
+        document.getElementById('directionForm').addEventListener('submit', function(event) {
+            event.preventDefault(); // Зупиняємо перезавантаження сторінки при відправці форми
 
-    // Отримуємо дані з форми
-    var title = document.getElementById('title').value;
-    var description = document.getElementById('description').value;
-    var email = document.getElementById('email').value;
+            // Отримуємо дані з форми
+            var group = document.getElementById('group').value;
+            var course = document.getElementById('course').value;
+            var title = document.getElementById('title').value;
+            var description = document.getElementById('description').value;
+            var email = document.getElementById('email').value;
 
-    // Генеруємо напрямок
-    var direction = "Заголовок: " + title + "<br>Опис: " + description + "<br>Адреса електронної пошти: " + email;
+            // Генеруємо напрямок
+            var direction = "<h3>Згенерований напрямок:</h3><p><strong>Група:</strong> " + group + "</p><p><strong>Курс:</strong> " + course + "</p><p><strong>Заголовок:</strong> " + title + "</p><p><strong>Опис:</strong> " + description + "</p><p><strong>Адреса електронної пошти:</strong> " + email + "</p>";
 
-    // Виводимо напрямок на сторінці
-    document.getElementById('output').innerHTML = "<h3>Згенерований напрямок:</h3>" + direction;
-});
+            // Додаємо напрямок на сторінку
+            document.getElementById('output').innerHTML = direction;
+        });
